29694b8299274671393cf93d0d740dfcf95efa1a
Björn Petersen <pheasn@gmail.com>
2019-12-01 14:45:26 +0100

    Fix test file naming

---
+++ test/01_test.dart
@@ -0,0 +1,30 @@
+import 'package:advent/solution/solutions.dart';
+import 'package:test/test.dart';
+
+Future<int> solveOne(List<String> input) async {
+  final advent = adventForDay(1)..init(input);
+  final solution = await advent.solveOne();
+  return int.parse(solution);
+}
+
+Future<int> solveTwo(List<String> input) async {
+  final advent = adventForDay(1)..init(input);
+  final solution = await advent.solveTwo();
+  return int.parse(solution);
+  }
+
+void main() {
+  group("part one", () {
+    test("for 12", () => expect(solveOne(["12"]), completion(2)));
+    test("for 14", () => expect(solveOne(["14"]), completion(2)));
+    test("for 1969", () => expect(solveOne(["1969"]), completion(654)));
+    test("for 100756", () => expect(solveOne(["100756"]), completion(33583)));
+    test("are summed", () => expect(solveOne(["12", "14"]), completion(4)));
+  });
+
+  group("part two", () {
+    test("for 14", () => expect(solveTwo(["14"]), completion(2)));
+    test("for 1969", () => expect(solveTwo(["1969"]), completion(966)));
+    test("for 100756", () => expect(solveTwo(["100756"]), completion(50346)));
+  });
+}
