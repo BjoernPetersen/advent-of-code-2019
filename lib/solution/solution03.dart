3f9899665bf03737af9201b184fc7199201d75e3
Björn Petersen <pheasn@gmail.com>
2019-12-03 07:31:29 +0100

    03 solution

---
+++ lib/solution/solution03.dart
@@ -0,0 +1,267 @@
+import 'dart:collection';
+import 'dart:math';
+
+import 'package:advent/advent.dart';
+
+class Solution3 extends Advent<Wire, int, int> {
+  @override
+  Wire readInputLine(String line) {
+    return Wire.fromLine(line);
+  }
+
+  @override
+  Future<int> solveOne() async {
+    final wire1 = input[0];
+    final wire2 = input[1];
+    final minX = min(wire1.minX, wire2.minX);
+    final minY = min(wire1.minY, wire2.minY);
+    final canvas = Canvas(
+      width: max(wire1.maxX, wire2.maxX) - minX + 1,
+      height: max(wire1.maxY, wire2.maxY) - minY + 1,
+      minX: minX,
+      minY: minY,
+    );
+
+    final zero = Point();
+    final intersections =
+        input.expand(canvas.add).where((it) => it != zero).toSet();
+
+    return intersections
+        .map((p) => p.distance(Point()))
+        .reduce((a, b) => min(a, b));
+  }
+
+  @override
+  Future<int> solveTwo() async {
+    final wire1 = input[0];
+    final wire2 = input[1];
+    final minX = min(wire1.minX, wire2.minX);
+    final minY = min(wire1.minY, wire2.minY);
+    final canvas = Canvas(
+      width: max(wire1.maxX, wire2.maxX) - minX + 1,
+      height: max(wire1.maxY, wire2.maxY) - minY + 1,
+      minX: minX,
+      minY: minY,
+    );
+
+    final zero = Point();
+    final intersections =
+        input.expand(canvas.add).where((it) => it != zero).toSet();
+
+    return intersections
+        .map((p) => _sumStepDistance(p, wire1, wire2))
+        .reduce((a, b) => min(a, b));
+  }
+}
+
+int _sumStepDistance(Point inter, Wire a, Wire b) {
+  return _stepDistance(inter, a) + _stepDistance(inter, b);
+}
+
+int _stepDistance(Point p, Wire w) {
+  var distance = 0;
+  for (final line in w.lines) {
+    if (line.contains(p)) {
+      distance += Line(line.start, p).length;
+      return distance;
+    } else {
+      distance += line.length;
+    }
+  }
+  throw ArgumentError();
+}
+
+class Canvas {
+  final int width, height;
+  final List<List<Set<Wire>>> _fields;
+  final int minX, minY;
+
+  Canvas({this.width, this.height, this.minX, this.minY})
+      : _fields = List(height);
+
+  Set<Point> add(Wire wire) {
+    Set<Point> overlap = Set();
+    for (final line in wire.lines) {
+      final start = line.start;
+      final end = line.end;
+      final startY = min(start.y, end.y);
+      final endY = max(start.y, end.y);
+      for (int y = startY; y <= endY; ++y) {
+        var row = _fields[y - minY];
+        if (row == null) {
+          row = List(width);
+          _fields[y - minY] = row;
+        }
+        final startX = min(start.x, end.x);
+        final endX = max(start.x, end.x);
+        for (int x = startX; x <= endX; ++x) {
+          Set<Wire> field = row[x - minX];
+          if (field == null) {
+            field = HashSet();
+            row[x - minX] = field;
+            field.add(wire);
+          } else if (field.add(wire)) {
+            overlap.add(Point(x: x, y: y));
+          }
+        }
+      }
+    }
+    return overlap;
+  }
+
+  Set<int> get(int x, int y) {
+    final row = _fields[y];
+    if (row == null) return Set();
+    return row[x] ?? Set();
+  }
+
+  @override
+  String toString() {
+    final buffer = StringBuffer();
+    for (int y = 0; y < height; ++y) {
+      final row = _fields[y];
+      for (int x = 0; x < width; ++x) {
+        if (row == null) {
+          buffer.write(".");
+        } else {
+          final wires = row[x];
+          if (wires == null) {
+            buffer.write(".");
+          } else if (wires.length > 1) {
+            buffer.write("X");
+          } else {
+            buffer.write("+");
+          }
+        }
+      }
+      buffer.writeln();
+    }
+    return buffer.toString();
+  }
+}
+
+class Line {
+  final Point start;
+  final Point end;
+
+  Line(this.start, this.end);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Line &&
+          runtimeType == other.runtimeType &&
+          start == other.start &&
+          end == other.end;
+
+  @override
+  int get hashCode => start.hashCode ^ end.hashCode;
+
+  @override
+  String toString() {
+    return '$start -> $end';
+  }
+
+  bool contains(Point p) {
+    final minX = min(start.x, end.x);
+    final minY = min(start.y, end.y);
+    final maxX = max(start.x, end.x);
+    final maxY = max(start.y, end.y);
+    return p.x >= minX && p.x <= maxX && p.y >= minY && p.y <= maxY;
+  }
+
+  int get length => start.distance(end).abs();
+}
+
+class Wire {
+  final Set<Line> lines;
+  final int minX;
+  final int minY;
+  final int maxX;
+  final int maxY;
+
+  Wire(this.lines, this.maxX, this.maxY, this.minX, this.minY);
+
+  factory Wire.fromLine(String line) {
+    final lines = Set<Line>();
+    int maxX = 0;
+    int maxY = 0;
+    int minX = 0;
+    int minY = 0;
+    var pos = Point(x: 0, y: 0);
+    for (final value in line.split(",")) {
+      final added = _line(pos, value);
+      lines.add(added);
+      pos = added.end;
+      maxX = max(pos.x, maxX);
+      maxY = max(pos.y, maxY);
+      minX = min(pos.x, minX);
+      minY = min(pos.y, minY);
+    }
+    return Wire(lines, maxX, maxY, minX, minY);
+  }
+
+  static Line _line(Point start, String value) {
+    final direction = value.substring(0, 1);
+    final length = int.parse(value.substring(1));
+    switch (direction) {
+      case "R":
+        return Line(start, start + Point(x: length));
+      case "U":
+        return Line(start, start - Point(y: length));
+      case "L":
+        return Line(start, start - Point(x: length));
+      case "D":
+        return Line(start, start + Point(y: length));
+    }
+  }
+}
+
+class Point {
+  final int x, y;
+
+  const Point({
+    this.x = 0,
+    this.y = 0,
+  });
+
+  Point operator +(Point other) => Point(x: x + other.x, y: y + other.y);
+
+  Point operator -(Point other) => Point(x: x - other.x, y: y - other.y);
+
+  int distance(Point other) {
+    return (x - other.x).abs() + (y - other.y).abs();
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Point &&
+          runtimeType == other.runtimeType &&
+          x == other.x &&
+          y == other.y;
+
+  @override
+  int get hashCode => x.hashCode ^ y.hashCode;
+
+  @override
+  String toString() {
+    return "($x, $y)";
+  }
+}
+
+extension on String {
+  Iterable<String> splitOn(final int splitOn) sync* {
+    for (var index = 0; index < this.length; ++index) {
+      final codeUnit = this.codeUnitAt(index);
+      if (codeUnit == splitOn) {
+        yield this.substring(0, index);
+        if (index != length - 1) {
+          yield* this.substring(index + 1).splitOn(splitOn);
+        }
+        return;
+      }
+    }
+    yield this;
+  }
+}
--- lib/solution/solutions.dart
+++ lib/solution/solutions.dart
@@ -1,6 +1,7 @@
 import 'package:advent/advent.dart';
 import 'package:advent/solution/solution01.dart';
 import 'package:advent/solution/solution02.dart';
+import 'package:advent/solution/solution03.dart';
 
 Advent adventForDay(int day) {
   switch (day) {
@@ -8,6 +9,8 @@ Advent adventForDay(int day) {
       return Solution1();
     case 2:
       return Solution2();
+    case 3:
+      return Solution3();
     default:
       return null;
   }
