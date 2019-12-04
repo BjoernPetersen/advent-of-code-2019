59b8f4964a79d0a1293ceb0f6f92c1e5a34fe464
Björn Petersen <pheasn@gmail.com>
2019-12-04 06:28:48 +0100

    Disable 02_test

--- test/02_test.dart
+++ test/02_test.dart
@@ -1,13 +1,15 @@
 import 'package:advent/advent.dart';
 import 'package:advent/solution/solutions.dart';
-import 'package:test/test.dart';
 
 Advent _advent(String input) {
   return adventForDay(2)..init([input]);
 }
 
 Future main() async {
+  /*
+  Only works without substitution at positions 1,2:
   group("part one", () {
     test("test", () => expect(_advent("1,9,10,3,2,3,11,0,99,30,40,50").solveOne(), completion(3500)));
   });
+   */
 }
