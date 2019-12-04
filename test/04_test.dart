59c5b2e2e5574a24928b4f41ad0fe623f1aa4933
Björn Petersen <pheasn@gmail.com>
2019-12-04 06:42:53 +0100

    Add solution checks for 04

--- test/04_test.dart
+++ test/04_test.dart
@@ -6,10 +6,24 @@ Advent _advent(String input) {
   return adventForDay(4)..init(["$input-$input"]);
 }
 
+Advent _adventRange(String input) {
+  return adventForDay(4)..init([input]);
+}
+
 Future main() async {
+  group("part one", () {
+    test(
+      "l1",
+      () => expect(_adventRange("264360-746325").solveOne(), completion(945)),
+    );
+  });
   group("part two", () {
     test("c0", () => expect(_advent("112233").solveTwo(), completion(1)));
     test("c1", () => expect(_advent("123444").solveTwo(), completion(0)));
     test("c2", () => expect(_advent("111122").solveTwo(), completion(1)));
+    test(
+      "l2",
+      () => expect(_adventRange("264360-746325").solveTwo(), completion(617)),
+    );
   });
 }
